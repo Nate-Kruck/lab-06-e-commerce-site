7ef1532e0720fea6ba322a5bbc2be127d34d6df1
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-07-13 15:26:29 -0700

    added product data for veggies.js

--- data/veggies.js
+++ data/veggies.js
@@ -1,15 +1,59 @@
 const tomato = {
     id: 'tomatoes',
     name: 'Cherry Tomato',
-    image: 'cherry.jpeg',
+    image: 'cherry.jpg',
     description: 'A small, juicy treat.',
     category: 'confused-fruit',
     price: 2.00,
     cost: 1.00
 };
 
+const cucumber = {
+    id: 'garden',
+    name: 'Cucumber',
+    image: 'cucumber.jpg',
+    description: 'mouth watering veggie, or fruit?',
+    category: 'confused-fruit',
+    price: 2.00,
+    cost: 1.00
+};
+
+const pepper = {
+    id: 'pepper',
+    name: 'Bell Pepper',
+    image: 'pepper.jpg',
+    description: 'Good to eat raw or cooked.',
+    category: 'confused-fruit',
+    price: 1.75,
+    cost: 0.50
+};
+
+const brocolli = {
+    id: 'brocolli',
+    name: 'Romanesco',
+    image: 'brocolli.jpg',
+    description: 'Unique Italian heirloom.',
+    category: 'veggie',
+    price: 3.75,
+    cost: 1.25
+};
+
+const onion = {
+    id: 'onion',
+    name: 'Walla Walla',
+    image: 'onion.jpg',
+    description: 'So good they will make you cry.',
+    category: 'veggie',
+    price: 2.50,
+    cost: 0.75
+};
+
 const veggies = [
-    tomato
+    tomato,
+    cucumber,
+    pepper,
+    brocolli,
+    onion
 ];
 
 export default veggies;
--- products/render-veggies.js
+++ products/render-veggies.js
@@ -1,23 +1,32 @@
 function renderVeggies(veggie) {
+
+    // creating vegetable list for html
     const li = document.createElement('li');
     li.className = veggie.category;
     li.title = veggie.description;
 
+    // adding images to each element
     const img = document.createElement('img');
     img.src = '../assets/' + veggie.image;
     img.alt = veggie.name + ' image';
     li.appendChild(img);
 
+    // add p element to contain the price and add button
     const p = document.createElement('p');
     p.className = 'price';
 
-    p.textContent = 'price ';
+    const money = '$' + veggie.price.toFixed(2);
 
+    // adds price text to rendered page
+    p.textContent = money + ' ';
+
+    // this is for the add button on the rendered page
     const button = document.createElement('button');
     button.textContent = 'Add';
     button.value = veggie.code;
     p.appendChild(button);
 
+    // nests the add button in the 'p' element so it appears on the same line
     li.appendChild(p);
 
     return li;
