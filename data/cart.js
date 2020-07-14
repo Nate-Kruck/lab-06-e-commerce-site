eb87a27da0f9d6c94ed80cca0ca826812ca96046
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-07-14 14:30:57 -0700

    added cart data in the form of array literals that contain object literals into a data/cart.js file

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,15 @@
+const cartItem = [{
+    id: 'tomatoes',
+    quantity: 4
+}, {
+    id: 'pepper',
+    quantity: 5
+}, {
+    id: 'brocolli',
+    quantity: 2
+}, {
+    id: 'onion',
+    quantity: 7
+}];
 
+export default cartItem;
